415baef83b82434f0568584367a51dc22a9a753f
B1229056 <144075644+GinoHuangs@users.noreply.github.com>
2024-06-06 09:07:10 +0800

    Update get value average in array.c

--- pratice/cs_1/get value average in array.c
+++ pratice/cs_1/get value average in array.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+double get_average(int arr[], int arr_size) {
+    double sum = 0.0;
+    for(int i; i<arr_size; i++) {
+        sum += arr[i];
+    }
+    return sum/arr_size;
+}
+
+int main() {
+    int balance[5] = {100, 2, 3, 17, 50};
+    double avg=0.0;
+    avg = get_average(balance, 5);
+    printf("Averge value is %.2f\n", avg);
+    return 0;
+}
