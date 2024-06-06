c24a068b4f2d035c6a44756f330c38d28dc6f55f
B1229056 <144075644+GinoHuangs@users.noreply.github.com>
2024-06-06 08:32:33 +0800

    Update char array2.c

--- pratice/cs_1/char array2.c
+++ pratice/cs_1/char array2.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+    char hello[] = "Hello";
+    for(int i =0; i<sizeof(hello); i++) {
+        printf("%d %c %d\n", i, hello[i], hello[i]);    //the last one is \0
+    }
+    return 0;
+}
